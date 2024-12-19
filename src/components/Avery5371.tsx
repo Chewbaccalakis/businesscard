592689cb37a3e075b45ec5993f048983e78905ec
Chewbaccalakis <nick@trochalakis.com>
2024-12-19 21:13:09 +0000

    probably not needed now that generatecard fixed?

--- src/components/Avery5371.tsx
+++ src/components/Avery5371.tsx
@@ -13,7 +13,7 @@ const rows = 5;
 
 // Configuration to adjust overall centering
 const config = {
-  xOffset: -5, // Horizontal offset for centering (positive shifts right, negative shifts left)
+  xOffset: 0, // Horizontal offset for centering (positive shifts right, negative shifts left)
   yOffset: 0, // Vertical offset for centering (positive shifts down, negative shifts up)
 };
 
