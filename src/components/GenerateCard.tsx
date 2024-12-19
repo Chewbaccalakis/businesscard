039e7f7d57c60497a73026e7324968760af2e912
Chewbaccalakis <nick@trochalakis.com>
2024-12-18 18:04:57 -0800

    first pass org

--- src/components/GenerateCard.tsx
+++ src/components/GenerateCard.tsx
@@ -2,21 +2,21 @@ import { PDFDocument, rgb } from 'pdf-lib';
 // @ts-ignore
 import * as fontkit from '@btielen/pdf-lib-fontkit';
 
+//
 // Configuration
+//
 
 // Page Size
 const width = 3.5 * 72;  // 3.5 inches = 252 points
 const height = 2 * 72;  // 2 inches = 144 points
 
-// Text
+// Static Items Config
 const header = `King County 4x4 Search and Rescue`;
+const logo = `/assets/logo.png`;
+const logocaption = `that others may live...`;
+const mailicon = `/assets/email.png`;
 const footnote1 = `P.O. Box 50785 • Bellevue, WA 98015`;
 const footnote2 = `100% Volunteer • Registered 501(c)(3) Non-Profit`;
-const logocaption = `that others may live...`;
-
-// Images
-const mailicon = `/assets/email.png`;
-const logo = `/assets/logo.png`;
 
 // Text Colors
 const HeaderColor = rgb(0, 0, 0);
@@ -24,7 +24,9 @@ const ContactInfoColor = rgb(92 / 255, 92 / 255, 92 / 255);
 const LogoCaptionColor = rgb(92 / 255, 92 / 255, 92 / 255);
 const FootnoteColor = rgb(92 / 255, 92 / 255, 92 / 255);
 
-//Fonts
+//
+// Code
+//
 
 const state = { headerX: 0, headerEndX: 0 }; // state for sides of header
 
@@ -46,7 +48,6 @@ const drawHeader = (
     page.drawText(text, { x: state.headerX, y, size, font, color });
 
   };
-
 
   // Draw Logo Function
   const drawLogo = async (
@@ -161,34 +162,25 @@ const drawFooter = async (
   
 
 export const GenerateCard = async (name: string, title: string, email: string, number: string) => {
-  console.log({ name, title, email, number });
 
   // Create PDF document
   const pdfDoc = await PDFDocument.create();
-
-
 
   // Font Loading
   pdfDoc.registerFontkit(fontkit)
   console.log('Registered fontkit')
 
+  // Arimo Font
   const arimoFontBytes = await fetch('/assets/arimo/ArimoSubset.woff').then((res) => res.arrayBuffer());
-  console.log('Arimo-Regular.ttf fetched successfully, bytes length:', arimoFontBytes.byteLength);
   const arimoFont = await pdfDoc.embedFont(arimoFontBytes, { subset: true }); // Loading a subset of a subset of a font aparently works???
-  console.log('Arimo-Regular font embedded successfully: ', arimoFont);
 
+  // Arimo Bold Font
   const arimoBoldBytes = await fetch('/assets/arimo/ArimoBoldSubset.woff').then((res) => res.arrayBuffer());
-  console.log('Arimo-Bold.ttf fetched successfully, bytes length:', arimoBoldBytes.byteLength);
   const arimoBold = await pdfDoc.embedFont(arimoBoldBytes, { subset: true });
-  console.log('Arimo-Bold font embedded successfully: ', arimoBold);
 
+  // Arimo Italic Font
   const arimoItalicBytes = await fetch('/assets/arimo/ArimoItalicSubset.woff').then((res) => res.arrayBuffer());
-  console.log('Arimo-Italic.ttf fetched successfully, bytes length:', arimoItalicBytes.byteLength);
   const arimoItalic = await pdfDoc.embedFont(arimoItalicBytes, { subset: true });
-  console.log('Arimo-Italic font embedded successfully: ', arimoItalic);
-
-
-
 
   // Page Setup
   const page = pdfDoc.addPage([width, height]);
